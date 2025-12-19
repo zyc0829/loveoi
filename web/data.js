d5142a26634acffe2bedc5aa4d29c8a6ce912c26
飞晨 <2983071582@qq.com>
2025-12-19 17:27:10 +0000

    第一次，做了个入口，能跳转三种模式，三个都能实现，算phase 1

---
+++ web/data.js
@@ -0,0 +1,17 @@
+window.NFC_DATA = {
+  song: {
+    songId: "1452409740",
+    title: "本周歌曲",
+    artist: "拍拍宇宙"
+  },
+
+  audio: {
+    src: "/audio/week1.mp3",
+    title: "群主寄语"
+  },
+
+  drama: {
+    title: "本周广播剧",
+    url: "https://s.rela.me/c/1SqTNu?album_id=111373&audio_id=116711"
+  }
+};
